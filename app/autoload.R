a69d5a04e218b3bda2f3823a1c57aecd6ca875f2
Alexandre de Souza <alexandre@aledsz.com.br>
2020-08-01 16:04:32 -0300

    Add script to autoload dependencies

---
+++ app/autoload.R
@@ -0,0 +1,26 @@
+options(stringsAsFactors = FALSE)
+
+dependencies <- readr::read_file("./DESCRIPTION")
+dependencies <- strsplit(dependencies, "\n", fixed = TRUE)[[1]]
+dependencies <- dependencies[grepl("    ", dependencies)]
+dependencies <- gsub(",", "", dependencies) 
+
+is_everything_installed <- jetpack::check()
+
+load_dependencies <- function (is_everything_installed) {
+	if (is_everything_installed) {
+		for (dep in dependencies) {
+			do.call("library", list(trimws(dep)))
+		}
+	} else {
+		jetpack::install()
+	}
+}
+
+load_dependencies(is_everything_installed)
+source("./app/app.R")
+
+app <- App$new()
+router <- app$get_router(app)
+
+source("resources/bootstrap.R")
